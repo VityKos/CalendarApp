9b43e82b72d6900b3237708f2fa16631fbccfbf7
VityKos <vik.kostyrin@gmail.com>
2021-10-21 21:31:02 +0300

    Modes

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ void Interface()
 {
     int mode;
     bool flag = true;
-    string modes = "1 mode: Create calendar;  2 mode: Test program; 3 mode: Exit";
+    string modes = "First mode: Create calendar;  Second mode: Test program; Third mode: Exit";
     while (flag)
     {
         cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" << endl;
