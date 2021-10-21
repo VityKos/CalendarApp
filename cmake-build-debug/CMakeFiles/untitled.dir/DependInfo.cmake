cd53f7fa85d3a072379c2cd24c1756903dff7429
VityKos <vik.kostyrin@gmail.com>
2021-10-21 21:29:50 +0300

    Try

--- cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/viktor/CLionProjects/CalendarApp_ProcedurEdition/main.cpp" "/Users/viktor/CLionProjects/CalendarApp_ProcedurEdition/cmake-build-debug/CMakeFiles/untitled.dir/main.cpp.o"
+  "/Users/viktor/Documents/GitHub/CalendarApp/main.cpp" "/Users/viktor/Documents/GitHub/CalendarApp/cmake-build-debug/CMakeFiles/untitled.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
